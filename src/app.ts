541360c5beb7f6de843145f778768a6f3f2f6ba5
Hosain <cryptichasan925@gmail.com>
2025-02-13 10:51:40 +0600

    inititally setup

---
+++ src/app.ts
@@ -0,0 +1,42 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import cockieParser from "cookie-parser";
+import path from "path";
+
+const app: Application = express();
+
+// parser
+app.use(express.json());
+app.use(cockieParser());
+
+// cors
+const allowedOrigins = ["http://localhost:3000"]; // Production frontend
+app.use(cors({ origin: allowedOrigins, credentials: true }));
+
+// Serve static files from the 'build' directory
+app.use(express.static(path.join(__dirname, "..", "build")));
+
+// Test route
+app.get("/", async (req: Request, res: Response) => {
+  const message = "Blood Bazar server is running";
+  res.send(message);
+});
+
+
+
+
+// application routes
+// app.use('/api', router);
+
+// Catch-all route for client-side routing
+app.get('*', (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+});
+
+// global error handler
+// app.use(globalErrorHandler);
+
+// not found route
+// app.use(notFound);
+
+export default app;
